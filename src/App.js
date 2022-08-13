28a6c07e6d8ea7a2d77531a89e99f9e1c336e283
gaurav-prusty <prustycolab@gmail.com>
2022-08-13 18:01:51 +0530

    test: deploy 3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import {Routes, Route} from "react-router-dom";
 import './App.css';
 import Home from "./pages/Home";
 import Favourites from "./pages/Favourites"; 
-import { useState } from "react";
 
 function App() {
 
